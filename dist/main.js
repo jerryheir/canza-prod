47f74b82aa5c2f698f8cbc125e6e92ae957f104a
Jeremiah Nwaeze <nwaezejerry@gmail.com>
2021-11-28 12:30:37 +0100

    Run build

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ const core_1 = require("@nestjs/core");
 const common_1 = require("@nestjs/common");
 const cookieParser = require("cookie-parser");
 const app_module_1 = require("./app.module");
-const PORT = process.env.port || 8000;
+const PORT = process.env.port || 8080;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
